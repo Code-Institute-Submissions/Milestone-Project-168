6013d7330f0a9b408de7b741155ab30832087389
Passion4film <passion4film@hotmail.co.uk>
2021-01-19 18:42:43 +0000

    JS to store the cards in frameworks and call on them - funtion to add/remove event listeners depending if card frameworks match

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,33 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        //do cards match?
+        if (firstCard.dataset.framework ===
+            secondCard.dataset.framework) {
+        // yes they match:
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', secondCard);
+        } else {
+            //no they dont match:
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1000);
+        }
+        
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
